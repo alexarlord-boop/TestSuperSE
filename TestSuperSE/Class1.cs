e4639d979a7885e5db3f93abd077b1046215ae24
alexarlord-boop <rezerw0.1@yandex.ru>
2021-11-27 13:48:33 +0300

    Добавьте файлы проекта.

---
+++ TestSuperSE/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocsVision.Platform.StorageServer.Extensibility;
+
+namespace TestSuperSE
+{
+    /// <summary>
+    /// Класс серверного расширения
+    /// </summary>
+    public class SEExtension : StorageServerExtension
+    {
+    }
+}
